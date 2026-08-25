afa136394c360efe7d5b1faa91a3f4a73e3a58c9
ilyabrosalin <brosalinio@gmail.com>
2026-08-25 16:25:10 +0300

    feat: add /start command with welcome message
    close #3

--- CurrencyBot/Program.cs
+++ CurrencyBot/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.Text.Json;
 using Telegram.Bot;
+using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 var json = File.ReadAllText("appsettings.json");
 var config = JsonDocument.Parse(json);
@@ -12,15 +14,33 @@ using var cts = new CancellationTokenSource();
 var bot = new TelegramBotClient(botToken, cancellationToken: cts.Token);
 var me = await bot.GetMe();
 
+bot.OnError += OnError;
 bot.OnMessage += OnMessage;
+bot.OnUpdate += OnUpdate;
 
 Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
 Console.ReadLine();
 cts.Cancel();
 
+async Task OnError(Exception exception, HandleErrorSource source)
+{
+    Console.WriteLine(exception);
+}
+
 async Task OnMessage(Message msg, UpdateType type)
 {
-    if (msg.Text is null) return;
-    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
-    await bot.SendMessage(msg.Chat, $"{msg.Text}");
+    if (msg.Text == "/start")
+    {
+        await bot.SendMessage(msg.Chat,
+            "Привет! Пока я в разработке — можешь написать мне что угодно, и я повторю это в ответ \n\nСкоро научусь считать выражения и конвертировать валюты!");
+    }
+    else
+    {
+        Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+        await bot.SendMessage(msg.Chat, $"{msg.Text}");
+    }
+}
+
+async Task OnUpdate(Update update)
+{
 }
