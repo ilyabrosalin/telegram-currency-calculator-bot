25de7b31fa18726f5a76cd2b5a90bbd62c0fa301
ilyabrosalin <brosalinio@gmail.com>
2026-08-25 18:48:31 +0300

    feat: implement Tokenizer with unit tests, refactoring pending

---
+++ CurrencyBot.Tests/TokenizerTests.cs
@@ -0,0 +1,74 @@
+﻿using CurrencyBot.Services;
+using Xunit;
+
+namespace CurrencyBot.Tests;
+
+public class TokenizerTests
+{
+    [Fact]
+    public void Tokenize_SimpleExpressionWithoutSpaces_ReturnsCorrectTokens()
+    {
+        var tokenizer = new Tokenizer();
+        var tokens = Tokenizer.Tokenize("5+3");
+
+        Assert.Equal(4, tokens.Count);
+        Assert.Equal(TokenType.Number, tokens[0].Type);
+        Assert.Equal("5", tokens[0].Value);
+        Assert.Equal(TokenType.Plus, tokens[1].Type);
+        Assert.Equal(TokenType.Number, tokens[2].Type);
+        Assert.Equal("3", tokens[2].Value);
+    }
+
+    [Fact]
+    public void Tokenize_SameExpressionWithSpaces_ReturnsIdenticalTokens()
+    {
+        var tokenizer = new Tokenizer();
+
+        var withoutSpaces = Tokenizer.Tokenize("5+3");
+        var withSpaces = Tokenizer.Tokenize("5 + 3");
+
+        Assert.Equal(withoutSpaces.Count, withSpaces.Count);
+        for (int i = 0; i < withoutSpaces.Count; i++)
+        {
+            Assert.Equal(withoutSpaces[i].Type, withSpaces[i].Type);
+            Assert.Equal(withoutSpaces[i].Value, withSpaces[i].Value);
+        }
+    }
+
+    [Fact]
+    public void Tokenize_MultiDigitNumber_ParsesAsSingleToken()
+    {
+        var tokenizer = new Tokenizer();
+        var tokens = Tokenizer.Tokenize("123+45");
+
+        Assert.Equal("123", tokens[0].Value);
+        Assert.Equal("45", tokens[2].Value);
+    }
+
+    [Fact]
+    public void Tokenize_DecimalNumber_ParsesAsSingleToken()
+    {
+        var tokenizer = new Tokenizer();
+        var tokens = Tokenizer.Tokenize("3.14+1");
+
+        Assert.Equal(TokenType.Number, tokens[0].Type);
+        Assert.Equal("3.14", tokens[0].Value);
+    }
+
+    [Fact]
+    public void Tokenize_InvalidCharacter_ThrowsFormatException()
+    {
+        var tokenizer = new Tokenizer();
+
+        Assert.Throws<FormatException>(() => Tokenizer.Tokenize("5 & 3"));
+    }
+
+    [Fact]
+    public void Tokenize_EmptyString_ReturnsEmptyTokenList()
+    {
+        var tokenizer = new Tokenizer();
+        var tokens = Tokenizer.Tokenize("");
+
+        Assert.Empty(tokens);
+    }
+}
---
+++ CurrencyBot/Services/Tokenizer.cs
@@ -0,0 +1,90 @@
+﻿namespace CurrencyBot.Services;
+
+public class Tokenizer
+{
+    public static List<Token> Tokenize(string text)
+    {
+        var tokens = new List<Token>();
+
+        var index = 0;
+
+        while (index < text.Length)
+        {
+            if (char.IsDigit(text[index]))
+            {
+                var start = index;
+                var hasSeparator = false;
+
+                while (index < text.Length)
+                {
+                    if (char.IsDigit(text[index]))
+                    {
+                        index++;
+                    }
+                    else if ((text[index] == '.' || text[index] == ',') && !hasSeparator &&
+                             (index + 1 < text.Length && char.IsDigit(text[index + 1])))
+                    {
+                        hasSeparator = true;
+                        index++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                var number = text.Substring(start, index - start).Replace(',', '.');
+
+                tokens.Add(new Token { Type = TokenType.Number, Value = number });
+                continue;
+            }
+
+            switch (text[index])
+            {
+                case '+':
+                    tokens.Add(new Token { Type = TokenType.Plus, Value = text[index].ToString() });
+                    break;
+                case '-':
+                    tokens.Add(new Token { Type = TokenType.Minus, Value = text[index].ToString() });
+                    break;
+                case '*':
+                    tokens.Add(new Token { Type = TokenType.Multiply, Value = text[index].ToString() });
+                    break;
+                case '/':
+                    tokens.Add(new Token { Type = TokenType.Divide, Value = text[index].ToString() });
+                    break;
+                case '(':
+                    tokens.Add(new Token { Type = TokenType.LeftParen, Value = text[index].ToString() });
+                    break;
+                case ')':
+                    tokens.Add(new Token { Type = TokenType.RightParen, Value = text[index].ToString() });
+                    break;
+                case ' ':
+                    break;
+                default:
+                    throw new FormatException();
+            }
+
+            index++;
+        }
+
+        return tokens;
+    }
+}
+
+public enum TokenType
+{
+    Number,
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    LeftParen,
+    RightParen
+}
+
+public class Token
+{
+    public TokenType Type { get; set; }
+    public string Value { get; set; }
+}
