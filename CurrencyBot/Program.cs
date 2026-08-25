3d50a43f9e9e605c31178e42088d779c3717f3b6
ilyabrosalin <brosalinio@gmail.com>
2026-08-25 15:37:33 +0300

    feat: implement echo bot with long polling
    close #2

--- CurrencyBot/Program.cs
+++ CurrencyBot/Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+var json = File.ReadAllText("appsettings.json");
+var config = JsonDocument.Parse(json);
+var botToken = config.RootElement.GetProperty("BotToken").GetString() ??
+               throw new InvalidOperationException("Bot token not found in configuration");
+
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient(botToken, cancellationToken: cts.Token);
+var me = await bot.GetMe();
+
+bot.OnMessage += OnMessage;
+
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
+Console.ReadLine();
+cts.Cancel();
+
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text is null) return;
+    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+    await bot.SendMessage(msg.Chat, $"{msg.Text}");
+}
