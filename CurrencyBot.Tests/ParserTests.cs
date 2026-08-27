757feaf3b9a034860bcef957cb01961f9b4b1529
ilyabrosalin <brosalinio@gmail.com>
2026-08-27 15:27:55 +0300

    feat: implement Shunting Yard parser with unit tests, close #5

---
+++ CurrencyBot.Tests/ParserTests.cs
@@ -0,0 +1,82 @@
+﻿using CurrencyBot.Services;
+
+public class ParserTests
+{
+    private static Token Num(string v) => new Token { Type = TokenType.Number, Value = v };
+    private static Token Op(TokenType t, string v) => new Token { Type = t, Value = v };
+    
+    private static List<string> Values(List<Token> tokens)
+    {
+        var result = new List<string>();
+        foreach (var t in tokens) result.Add(t.Value);
+        return result;
+    }
+
+    [Fact]
+    public void SingleNumber_ReturnsSameNumber()
+    {
+        // "5" -> "5"
+        var tokens = new List<Token> { Num("5") };
+
+        var rpn = Parser.ToRpn(tokens);
+
+        Assert.Equal(new List<string> { "5" }, Values(rpn));
+    }
+
+    [Fact]
+    public void SimpleAddition_ReturnsOperandsThenOperator()
+    {
+        // "5 + 3" -> "5 3 +"
+        var tokens = new List<Token>
+        {
+            Num("5"), Op(TokenType.Plus, "+"), Num("3")
+        };
+
+        var rpn = Parser.ToRpn(tokens);
+
+        Assert.Equal(new List<string> { "5", "3", "+" }, Values(rpn));
+    }
+
+    [Fact]
+    public void MultiplicationHasHigherPrecedenceThanAddition()
+    {
+        // "5 + 3 * 2" -> "5 3 2 * +"
+        var tokens = new List<Token>
+        {
+            Num("5"), Op(TokenType.Plus, "+"), Num("3"), Op(TokenType.Multiply, "*"), Num("2")
+        };
+
+        var rpn = Parser.ToRpn(tokens);
+
+        Assert.Equal(new List<string> { "5", "3", "2", "*", "+" }, Values(rpn));
+    }
+
+    [Fact]
+    public void Parentheses_OverridePrecedence()
+    {
+        // "(5 + 3) * 2" -> "5 3 + 2 *"
+        var tokens = new List<Token>
+        {
+            Op(TokenType.LeftParen, "("), Num("5"), Op(TokenType.Plus, "+"), Num("3"), Op(TokenType.RightParen, ")"),
+            Op(TokenType.Multiply, "*"), Num("2")
+        };
+
+        var rpn = Parser.ToRpn(tokens);
+
+        Assert.Equal(new List<string> { "5", "3", "+", "2", "*" }, Values(rpn));
+    }
+
+    [Fact]
+    public void SamePrecedenceOperators_AreLeftAssociative()
+    {
+        // "10 - 5 - 2" -> "10 5 - 2 -"
+        var tokens = new List<Token>
+        {
+            Num("10"), Op(TokenType.Minus, "-"), Num("5"), Op(TokenType.Minus, "-"), Num("2")
+        };
+
+        var rpn = Parser.ToRpn(tokens);
+
+        Assert.Equal(new List<string> { "10", "5", "-", "2", "-" }, Values(rpn));
+    }
+}
---
+++ CurrencyBot/Services/Parser.cs
@@ -0,0 +1,72 @@
+﻿namespace CurrencyBot.Services;
+
+public class Parser
+{
+    public static List<Token> ToRpn(List<Token> tokens)
+    {
+        var result = new List<Token>();
+        var stack = new Stack<Token>();
+
+        foreach (var token in tokens)
+        {
+            switch (token.Type)
+            {
+                case TokenType.Number:
+                    result.Add(token);
+                    break;
+                case TokenType.Plus:
+                case TokenType.Minus:
+                case TokenType.Multiply:
+                case TokenType.Divide:
+                    if (stack.Count == 0)
+                    {
+                        stack.Push(token);
+                    }
+                    else if (GetPriority(stack.Peek().Type) >= GetPriority(token.Type))
+                    {
+                        result.Add(stack.Pop());
+                        stack.Push(token);
+                    }
+                    else if (GetPriority(stack.Peek().Type) <= GetPriority(token.Type))
+                    {
+                        stack.Push(token);
+                    }
+                    break;
+                case TokenType.LeftParen:
+                    stack.Push(token);
+                    break;
+                case TokenType.RightParen:
+                    while (stack.Count > 0)
+                    {
+                        if (stack.Peek().Type == TokenType.LeftParen)
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            result.Add(stack.Pop());
+                        }
+                    }
+
+                    break;
+            }
+        }
+
+        foreach (var token in stack)
+        {
+            result.Add(token);
+        }
+
+        return result;
+    }
+
+    private static int GetPriority(TokenType type)
+    {
+        return type switch
+        {
+            TokenType.Multiply or TokenType.Divide => 2,
+            TokenType.Plus or TokenType.Minus => 1,
+            _ => 0
+        };
+    }
+}
